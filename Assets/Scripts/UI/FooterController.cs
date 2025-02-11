afef3687f2df890f3394112c53dfa4cbbd7e8649
TonySenpai04 <duongvansu006@gmail.com>
2025-02-11 16:18:47 +0700

    add library UI

---
+++ Assets/Scripts/UI/FooterController.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[Serializable]
+public struct ButtonFotter 
+{
+    public string name;
+    public Button button;
+    public Sprite selected;
+    public Sprite unselected;
+}
+
+public class FooterController : MonoBehaviour
+{
+   public List<ButtonFotter> buttons;
+    private void Start()
+    {
+      foreach (var button in buttons)
+       {
+            button.button.GetComponent<Image>().sprite = button.unselected;
+            button.button.onClick.AddListener(() => OnButtonClicked(button.button));
+       }
+        SelectButton("Map");
+    }
+    private void OnButtonClicked(Button clickedButton)
+    {
+        foreach (var button in buttons)
+        {
+            Image buttonImage = button.button.GetComponent<Image>();
+            if (button.button == clickedButton)
+            {
+                buttonImage.sprite = button.selected;
+            }
+            else
+            {
+                buttonImage.sprite = button.unselected;
+            }
+        }
+    }
+    public void SelectButton(string name)
+    {
+        foreach (var button in buttons)
+        {
+            Image buttonImage = button.button.GetComponent<Image>();
+            if (button.name == name)
+            {
+                buttonImage.sprite = button.selected;
+            }
+            else
+            {
+                buttonImage.sprite = button.unselected;
+            }
+        }
+    }
+}
