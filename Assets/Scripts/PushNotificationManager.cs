073be2fd0a7cc6298e3fb341bd66e108dfb4b746
TonySenpai04 <duongvansu006@gmail.com>
2025-02-27 18:24:29 +0700

    Push Noti and add Firebase

---
+++ Assets/Scripts/PushNotificationManager.cs
@@ -0,0 +1,23 @@
+﻿using Firebase.Messaging;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushNotificationManager : MonoBehaviour
+{
+    void Start()
+    {
+        FirebaseMessaging.TokenReceived += OnTokenReceived;
+        FirebaseMessaging.MessageReceived += OnMessageReceived;
+    }
+
+    void OnTokenReceived(object sender, Firebase.Messaging.TokenReceivedEventArgs token)
+    {
+        Debug.Log("FCM Token: " + token.Token);
+    }
+
+    void OnMessageReceived(object sender, Firebase.Messaging.MessageReceivedEventArgs e)
+    {
+        Debug.Log("Thông báo nhận được: " + e.Message.Notification.Body);
+    }
+}
